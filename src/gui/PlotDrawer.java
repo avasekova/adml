dfbfc07af54402dfb4e3785f5d970ea49158fe18
Andrejka <avaseko@gmail.com>
2014-09-18 16:58:57 +0200

    plotting now the original data as well (in black, and the other - fitted - plots in blue, for now).

--- src/gui/PlotDrawer.java
+++ src/gui/PlotDrawer.java
@@ -9,6 +9,7 @@ import utils.Utils;
 
 public class PlotDrawer {
     
+    //TODO generovat i legendu do toho vysledneho grafu!
     public static void drawPlots(int width, int height, List<Double> allData, int numForecasts, List<TrainAndTestReport> reports) {
         //plot.ts(brent$Center, ylim=range(c(brent.center.nnet.4cast, brent$Center)), xlim=range(c(brent.center.nnet.4cast,brent$Center)), col="blue")
         if (reports.isEmpty()) {
@@ -46,10 +47,14 @@ public class PlotDrawer {
             }
             
             StringBuilder plotCode = new StringBuilder(r.getFittedValuesPlotCode());
-            plotCode.insert(r.getFittedValuesPlotCode().length() - 1, ", xlim = " + rangesX + ", ylim = " + rangesY);
+            plotCode.insert(r.getFittedValuesPlotCode().length() - 1, ", xlim = " + rangesX + ", ylim = " + rangesY + ", col=\"blue\"");
             //TODO neskor zarovnavat forecasty doprava, pretoze niekto tam zobrazuje aj realne data a niekto nie, ALEBO zobrazit u vsetkych aj realne data aj forecast
             rengine.eval(plotCode.toString());
         }
+        
+        rengine.assign("all.data", Utils.listToArray(allData));
+        rengine.eval("par(new=TRUE)");
+        rengine.eval("plot.ts(all.data, xlim = " + rangesX + ", ylim = " + rangesY + ")");
 
         MainFrame.gdCanvas.setSize(new Dimension(width, height)); //TODO nechce sa zmensit pod urcitu velkost, vymysliet
         MainFrame.gdCanvas.initRefresh();
