e7b62986756f38e4496486a0bd400c9283e11445
Andrejka <avaseko@gmail.com>
2015-06-27 19:55:33 +0200

    colours from D3js, not used

--- src/gui/ColourService.java
+++ src/gui/ColourService.java
@@ -3,12 +3,33 @@ public class ColourService {
     
     //TODO vykradnut nieco ako:
     ///https://github.com/mbostock/d3/blob/master/lib/colorbrewer/colorbrewer.js
-    ///https://github.com/mbostock/d3/blob/master/lib/colorbrewer/colorbrewer.css
     ///https://github.com/mbostock/d3/wiki/Ordinal-Scales
     
     private static ColourService instance = null;
     private int counter = -1;
     private static final String[] COLOURS = new String[]{ //TODO vybrat sem nejake pekne! (rucne) - a hlavne viac
+//        //farby z D3js:
+//        "#1F77B4",
+//        "#FF7F0E",
+//        "#2CA02C",
+//        "#D62728",
+//        "#9467BD",
+//        "#8C564B",
+//        "#E377C2",
+//        "#7F7F7F",
+//        "#BCBD22",
+//        "#17BECF",
+//        "#AEC7E8",
+//        "#FFBB78",
+//        "#98DF8A",
+//        "#FF9896",
+//        "#C5B0D5",
+//        "#C49C94",
+//        "#F7B6D2",
+//        "#C7C7C7",
+//        "#DBDB8D",
+//        "#9EDAE5",
+        
         //TODO urgentne pridat viac farieb, aby to nebolo treba modulit!
         "#FF00FF",
         "#0000FF",
