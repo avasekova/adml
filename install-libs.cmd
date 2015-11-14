4a9084a55316f1a5055b16cb5bb71f266ac4d5de
Dusan Klinec <dusan.klinec@gmail.com>
2015-11-14 17:36:44 +0100

    paralel-maven: fixing windows script for installation of local JAR dependencies to maven

--- install-libs.cmd
+++ install-libs.cmd
@@ -1,50 +1,50 @@
 SETLOCAL ENABLEEXTENSIONS
 SET ME=%~n0
 SET PARENT=%~dp0
-SET REPO=%PARENT%/libraries-repo
-SET LIBS=%PARENT%/libraries
+SET REPO=%PARENT%\libraries-repo
+SET LIBS=%PARENT%\libraries
 
-mvn install:install-file \
--Dfile=%LIBS%/javaGD.jar  \
--DgroupId=mlp.org.rosuda \
--DartifactId=javaGD      \
--Dversion=1.0            \
--Dpackaging=jar          \
--DgeneratePom=true       \
+mvn install:install-file ^
+-Dfile=%LIBS%\javaGD.jar ^
+-DgroupId=mlp.org.rosuda ^
+-DartifactId=javaGD      ^
+-Dversion=1.0            ^
+-Dpackaging=jar          ^
+-DgeneratePom=true       ^
 -DlocalRepositoryPath=%REPO%
 
-mvn install:install-file \
--Dfile=%LIBS%/JRI.jar     \
--DgroupId=mlp.org.rosuda \
--DartifactId=JRI         \
--Dversion=1.0            \
--Dpackaging=jar          \
--DgeneratePom=true       \
+mvn install:install-file ^
+-Dfile=%LIBS%\JRI.jar    ^
+-DgroupId=mlp.org.rosuda ^
+-DartifactId=JRI         ^
+-Dversion=1.0            ^
+-Dpackaging=jar          ^
+-DgeneratePom=true       ^
 -DlocalRepositoryPath=%REPO%
 
-mvn install:install-file \
--Dfile=%LIBS%/JRIEngine.jar \
--DgroupId=mlp.org.rosuda \
--DartifactId=JRIEngine   \
--Dversion=1.0            \
--Dpackaging=jar          \
--DgeneratePom=true       \
+mvn install:install-file ^
+-Dfile=%LIBS%\JRIEngine.jar ^
+-DgroupId=mlp.org.rosuda ^
+-DartifactId=JRIEngine   ^
+-Dversion=1.0            ^
+-Dpackaging=jar          ^
+-DgeneratePom=true       ^
 -DlocalRepositoryPath=%REPO%
 
-mvn install:install-file \
--Dfile=%LIBS%/REngine.jar \
--DgroupId=mlp.org.rosuda \
--DartifactId=REngine     \
--Dversion=1.0            \
--Dpackaging=jar          \
--DgeneratePom=true       \
+mvn install:install-file  ^
+-Dfile=%LIBS%\REngine.jar ^
+-DgroupId=mlp.org.rosuda ^
+-DartifactId=REngine     ^
+-Dversion=1.0            ^
+-Dpackaging=jar          ^
+-DgeneratePom=true       ^
 -DlocalRepositoryPath=%REPO%
 
-mvn install:install-file \
--Dfile=%LIBS%/jxl.jar     \
--DgroupId=mlp.jxl        \
--DartifactId=jxl         \
--Dversion=1.0            \
--Dpackaging=jar          \
--DgeneratePom=true       \
+mvn install:install-file  ^
+-Dfile=%LIBS%\jxl.jar     ^
+-DgroupId=mlp.jxl        ^
+-DartifactId=jxl         ^
+-Dversion=1.0            ^
+-Dpackaging=jar          ^
+-DgeneratePom=true       ^
 -DlocalRepositoryPath=%REPO%
