50c7b99b6f9a5e0837f2b84bd284e4f46fdc7c2c
avasekova <avaseko@gmail.com>
2016-07-19 21:26:40 +0200

    guiUtils - bulk processing of swing components

---
+++ src/main/java/gui/GUIUtils.java
@@ -0,0 +1,43 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class GUIUtils {
+
+    public static void setComponentsEnabled(JFrame parent, Class<? extends Component> childType, boolean enabled) {
+        setComponentsEnabled(parent.getRootPane(), childType, enabled);
+    }
+
+    public static void setComponentsEnabled(JDialog parent, Class<? extends Component> childType, boolean enabled) {
+        setComponentsEnabled(parent.getRootPane(), childType, enabled);
+    }
+
+    public static void setComponentsEnabled(JComponent parent, Class<? extends Component> childType, boolean enabled) {
+        setComponentsEnabled(parent, c -> childType.isInstance(c), enabled);
+    }
+
+    public static void setComponentsEnabled(JComponent parent, Predicate<JComponent> filter, boolean enabled) {
+        setComponentsEnabled(parent, filter, c -> c.setEnabled(enabled));
+    }
+
+    public static void setComponentsEnabled(JComponent parent, Predicate<JComponent> filter, Consumer<JComponent> action) {
+        getAllComponents(parent).stream().filter(filter).forEach(action);
+    }
+
+    public static List<JComponent> getAllComponents(JComponent component) {
+        List<JComponent> components = new ArrayList<>();
+        for (Component c : component.getComponents()) {
+            if (c instanceof JComponent) {
+                components.add((JComponent) c);
+                components.addAll(getAllComponents((JComponent) c));
+                //do not process JWindows or JDialogs
+            }
+        }
+        return components;
+    }
+}
